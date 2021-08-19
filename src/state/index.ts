8f5df266c64fee8bbe4964314f3c398e565f7f36
tada1765 <www.tada1765@gmail.com>
2021-08-19 14:49:32 +0800

    initial commit

---
+++ src/state/index.ts
@@ -0,0 +1,6 @@
+// import all files, so App only import this file
+
+// export all from action-creators:
+export * as actionCreators from "./action-creators"
+export * from './store'
+export * from "./reducers/index"
